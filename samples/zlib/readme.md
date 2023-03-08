9db4068025f658bd95900c3b51d5f1dbf5c6e77c
huanle0610 <huanle0610@qq.com>
2023-03-08 12:54:00 +0000

    run in codespace

---
+++ samples/zlib/readme.md
@@ -0,0 +1,11 @@
+## how to run
+
+1. sdk list gradle
+
+```sh
+sdk rm gradle 8.0.2
+sdk install gradle 7.6
+
+cd samples/zlib
+gradle test
+```
--- samples/zlib/src/test/java/org/bytedeco/zlib/UnitTest.java
+++ samples/zlib/src/test/java/org/bytedeco/zlib/UnitTest.java
@@ -2,10 +2,12 @@ package org.bytedeco.zlib;
 
 import org.junit.Test;
 import static org.bytedeco.zlib.global.zlib.*;
+import static org.junit.Assert.assertEquals;
 
 public class UnitTest {
     @Test public void test() {
         System.out.println(zlibVersion().getString());
+        assertEquals("1.2.13", zlibVersion().getString());
     }
 }
 
